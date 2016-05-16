0cc1ac1216e186ad7a143d121f253f3a31bbe3a5
Koreleone <773781556@qq.com>
2016-05-16 10:57:06 +0800

    66

--- app/src/main/java/com/example/administrator/getpet/ui/Me/PersonalActivity.java
+++ app/src/main/java/com/example/administrator/getpet/ui/Me/PersonalActivity.java
@@ -4,34 +4,16 @@ import android.os.Bundle;
 
 import com.example.administrator.getpet.R;
 import com.example.administrator.getpet.base.BaseActivity;
-import com.example.administrator.getpet.bean.UserBean;
-import com.example.administrator.getpet.presenter.UserPresenter;
 
-public class PersonalActivity extends BaseActivity implements IUserView {
 
-    private UserPresenter userPresenter;
+public class PersonalActivity extends BaseActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal);
-        initView();
 
-    }
 
-    private void initView() {
-        userPresenter = new UserPresenter(this);
-    }
-
-    @Override
-    public void setUser(UserBean user) {
-
-    }
-
-    @Override
-    public UserBean getUser() {
-        UserBean user = new UserBean();
-
-        return user;
     }
 }
