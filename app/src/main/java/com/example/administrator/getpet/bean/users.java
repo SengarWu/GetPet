75f4accabb159189ad34361d35a9a663d1d6ecc0
76829913@qq.com <76829913@qq.com>
2016-05-16 15:06:35 +0800

    5.16-6

--- app/src/main/java/com/example/administrator/getpet/bean/users.java
+++ app/src/main/java/com/example/administrator/getpet/bean/users.java
@@ -3,15 +3,19 @@ package com.example.administrator.getpet.bean;
 import java.io.Serializable;
 public class users  implements Serializable {
 
-public String id;               //id
-public String phone;            //手机号
-public String nickName;         //昵称
-public String sex;              //性别
-public int age;                 //年龄
-public String address;          //所在地
-public String email;            //邮箱
-public String personal;         //个人说明
-public String occupation;       //职业
-public indentified indentified; //是否认证
-public int user_reputation;     //声望
+    public String id;               //id
+    public String phone;            //手机号
+    public String nickName;         //昵称
+    public String sex;              //性别
+    public int age;                 //年龄
+    public String address;          //所在地
+    public String email;            //邮箱
+    public String personal;         //个人说明
+    public String occupation;       //职业
+    public indentified indentified; //是否认证
+    /**
+     * 用户类型0为普通用户，1为救助站用户
+     */
+    public int indentify;
+    public int user_reputation;     //声望
 }
---
+++ app/src/main/java/com/example/administrator/getpet/ui/Me/EditIntroduceActivity.java
@@ -0,0 +1,15 @@
+package com.example.administrator.getpet.ui.Me;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.administrator.getpet.R;
+
+public class EditIntroduceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_introduce);
+    }
+}
