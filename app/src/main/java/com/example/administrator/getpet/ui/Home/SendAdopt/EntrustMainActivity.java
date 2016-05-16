a197a43e5fe869527ab9020bb7586689653770a1
Koreleone <773781556@qq.com>
2016-05-16 23:56:21 +0800

    667

--- app/src/main/java/com/example/administrator/getpet/ui/Home/SendAdopt/EntrustMainActivity.java
+++ app/src/main/java/com/example/administrator/getpet/ui/Home/SendAdopt/EntrustMainActivity.java
@@ -9,6 +9,7 @@ import android.widget.ImageView;
 import com.example.administrator.getpet.R;
 import com.example.administrator.getpet.base.BaseActivity;
 import com.example.administrator.getpet.bean.entrust;
+import com.example.administrator.getpet.ui.Home.SendAdopt.Adapter.MyEntrustListAdapter;
 import com.example.administrator.getpet.view.xlistview.SimpleFooter;
 import com.example.administrator.getpet.view.xlistview.SimpleHeader;
 import com.example.administrator.getpet.view.xlistview.ZrcListView;
@@ -21,7 +22,8 @@ public class EntrustMainActivity extends BaseActivity implements View.OnClickLis
     private ArrayList<entrust> items = new ArrayList<>();
     private ZrcListView listView;
     private Handler handler;//用于接收子线程的信息以刷新主线程
-
+    private MyEntrustListAdapter adapter;//寄养信息列表的适配器，用于向列表填充数据
+    private int curPage;//当前的页码
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,13 +53,27 @@ public class EntrustMainActivity extends BaseActivity implements View.OnClickLis
         listView.setItemAnimForTopIn(R.anim.top_item_in);
         listView.setItemAnimForBottomIn(R.anim.bottom_item_in);
 
-        /*adapter = new SeekHelpAdapter(this, items);
-        listView.setAdapter(adapter);*/
-
+        adapter = new MyEntrustListAdapter(this, items);
+        listView.setAdapter(adapter);
         if (items.size() <= 0)
         {
             listView.refresh(); // 主动下拉刷新
         }
+
+        // 下拉刷新事件回调
+        listView.setOnRefreshStartListener(new ZrcListView.OnStartListener() {
+            @Override
+            public void onStart() {
+                refresh();
+            }
+        });
+        // 加载更多事件回调
+        listView.setOnLoadMoreStartListener(new ZrcListView.OnStartListener() {
+            @Override
+            public void onStart() {
+                loadMore();
+            }
+        });
     }
 
     @Override
@@ -70,5 +86,33 @@ public class EntrustMainActivity extends BaseActivity implements View.OnClickLis
             case R.id.back:
                 break;
         }
+    }
+
+    private void refresh() {
+        curPage=0;
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                QueryEntrust();//queryProxy为自定义的查询类
+            }
+        });
+    }
+    private void loadMore() {
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                QueryCountsh();
+            }
+        });
+    }
+
+    private void QueryCountsh() {
+    }
+
+    private void QueryEntrust() {
+
+    }
+    private void Querymore(int page) {
+
     }
 }
