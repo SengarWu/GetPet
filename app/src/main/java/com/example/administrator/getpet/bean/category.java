d1f2f7327a2ed88a6ed3e67db91a0adffadf8135
Koreleone <773781556@qq.com>
2016-05-16 11:07:49 +0800

    66

---
+++ app/src/main/java/com/example/administrator/getpet/bean/category.java
@@ -0,0 +1,9 @@
+package com.example.administrator.getpet.bean;
+
+import java.io.Serializable;
+import java.util.Date;
+public class category  implements Serializable {
+
+public String id;
+public String name;
+}
